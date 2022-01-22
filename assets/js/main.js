2acf7b7d5f4368163957eaa9fad38a535ce5eb3f
SuperCoder <supercoder@AirvonSperCoder.fritz.box>
2022-01-22 19:54:38 +0100

    malzwei

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+let num = document.getElementById("num");
+let solution = document.getElementById("result");
+
+function malzwei() {
+    let result = num.value * 2;
+    solution.innerHTML = result;
+}
+
